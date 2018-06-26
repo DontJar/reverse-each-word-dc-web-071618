0487341473da480641e2cdf567f4078f064afbea
Jon Schulman <schulman99@gmail.com>
2018-06-26 01:17:14 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   new_string = []
   array = string.split(' ')
   array.each do |flip|
-    new_string = flip.reverse
+    new_string << flip.reverse
   end
   new_string
 end
